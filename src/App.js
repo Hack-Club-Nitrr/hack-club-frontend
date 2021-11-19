2ff48203173117a8b5817adc54a477e4508f2377
Your Name <you@example.com>
2021-11-19 21:16:29 +0530

    added main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './assets/stylesheets/app.css';
 import Topnav from './components/Topnav';
-
+import Main from './components/Main'
 const App = () => {
     return (
         <div className="container">
             <Topnav/>
-
+            <Main/>
         </div>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,19 @@
+
+import React from "react";
+import "../assets/stylesheets/main.css";
+import Image from "../assets/image/web-development.png"
 const Main = () => {
+    
+
     return (
         <div>
-            
+            <div className="title">
+              <h1>Hack</h1>
+              <h3>Club</h3>
+            </div> 
+            <img className ="title-img"src = {Image} alt = "intro" width = "50%"/>
         </div>
     )
 }
 
-export default Main
+export default Main;
