c17e08527642a3aa8d3152a33860ccfaf46542f4
Your Name <you@example.com>
2021-11-29 23:43:49 +0530

    trail

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,11 +8,10 @@ const Main = () => {
     return (
         <div className = "main-page">
             <div className="title">
-              <h1>Hack</h1>
-              <h3>Club</h3>
-              <h5>NITRR</h5>
+              <h1>Hack Club</h1>
+              <h3>NITRR</h3>
               <br />
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ipiscing elit, sed do eiusmod tempor </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
             <img className = "title-img" src = {Image} alt = "intro" width = "50%"/>
         </div>
