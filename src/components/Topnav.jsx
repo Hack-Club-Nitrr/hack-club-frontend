a3336e4889863247a54ca2175b72e2f81900434b
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2021-12-14 03:30:38 +0530

    navbar

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../assets/stylesheets/topnav.css";
 
+
+
 const Topnav = () => {
   var status = false;
   const openMenu = () => {
@@ -42,6 +44,24 @@ const Topnav = () => {
       }, 500);
     }
   };
+  const ss =(str)=>{
+    const arr= str.map(function(i){
+      let ist = Array.from(i)
+      let grr = ist.map(function(j){
+        return( 
+          <div className="letter">{j}</div>
+        )
+      })
+      return(
+        <div className="navlink">{grr}</div>
+      )
+    })
+    return(
+      <div className="wrap_content"><div className="snv">{arr}</div></div>
+    )
+  }
+
+  let arr = [ss(["Home","About","Contacts","Hakathons","News"])]
   return (
     <div style={{ overflowX: "hidden" }}>
       <button
@@ -57,47 +77,8 @@ const Topnav = () => {
       </button>
       <div className="wrap_out">
         <div className="wrap_in"></div>
-
-        <div className="wrap_content">
-          <div className="navlink">
-            <div className="letter">H</div>
-            <div className="letter">o</div>
-            <div className="letter">m</div>
-            <div className="letter">e</div>
-          </div>
-          <div className="navlink">
-            <div className="letter">A</div>
-            <div className="letter">b</div>
-            <div className="letter">o</div>
-            <div className="letter">u</div>
-            <div className="letter">t</div>
-          </div>
-          <div className="navlink">
-            <div className="letter">P</div>
-            <div className="letter">r</div>
-            <div className="letter">o</div>
-            <div className="letter">j</div>
-            <div className="letter">e</div>
-            <div className="letter">c</div>
-            <div className="letter">t</div>
-            <div className="letter">s</div>
-          </div>
-          <div className="navlink">
-            <div className="letter">C</div>
-            <div className="letter">o</div>
-            <div className="letter">n</div>
-            <div className="letter">t</div>
-            <div className="letter">a</div>
-            <div className="letter">c</div>
-            <div className="letter">t</div>
-          </div>
-          <div className="navlink">
-            <div className="letter">b</div>
-            <div className="letter">l</div>
-            <div className="letter">o</div>
-            <div className="letter">g</div>
-          </div>
-        </div>
+        {arr}
+        
       </div>
     </div>
   );
