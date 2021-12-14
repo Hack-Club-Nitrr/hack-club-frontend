ff4f26524a97fe3068e86000103ca938398f2134
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2021-12-14 12:22:04 +0530

    navbar animation on scroll

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+let j = document.documentElement.scrollTop;
+document.onscroll = () => {
+  let h = window.innerHeight;
+   if (document.documentElement.scrollTop > j + h/50){
+    console.log("df"); 
+    let f= document.querySelector(".wrap_out")
+    f.style.top = "-145%" 
+   } else if( document.documentElement.scrollTop < j - h/50){
+    console.log("df"); 
+    let f= document.querySelector(".wrap_out")
+    f.style.top = "0%" 
+   }
+   
+j = document.documentElement.scrollTop; 
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
