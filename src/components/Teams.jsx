32c17468fafae6c5ec71659f74e672e6fd77a157
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-12-21 17:21:51 +0530

    team divisions reversed

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -29,126 +29,38 @@ const Teams = () => {
     }
     const Team = TeamData.map((member, i) => {
       if (i === 0) return "";
-      if (i > 4) {
-        return (
-          <div className="col-md-3" key={i}>
-            <div className="team_card">
-              <div className="team_img_wrap_outer">
-                <div className="team_img_wrap">
-                  <a target="_blank" href={member[5]} rel="noreferrer">
-                    <div className="team_social team_linkedin">
-                      <i className="fa fa-linkedin"></i>
-                    </div>
-                  </a>
-                  <a target="_blank" href={member[4]} rel="noreferrer">
-                    <div className="team_social team_insta">
-                      <i className="fa fa-instagram"></i>
-                    </div>
-                  </a>
-                  <a target="_blank" href={member[6]} rel="noreferrer">
-                    <div className="team_social team_email">
-                      <i className="fa fa-envelope"></i>
-                    </div>
-                  </a>
-                  <img
-                    className="team_img"
-                    src={`https://gdurl.com${member[3]}`}
-                    alt="."
-                  />
-                </div>
+      return (
+        <div className="col-md-3" key={i}>
+          <div className="team_card">
+            <div className="team_img_wrap_outer">
+              <div className="team_img_wrap">
+                <a target="_blank" href={member[5]} rel="noreferrer">
+                  <div className="team_social team_linkedin">
+                    <i className="fa fa-linkedin"></i>
+                  </div>
+                </a>
+                <a target="_blank" href={member[4]} rel="noreferrer">
+                  <div className="team_social team_insta">
+                    <i className="fa fa-instagram"></i>
+                  </div>
+                </a>
+                <a target="_blank" href={member[6]} rel="noreferrer">
+                  <div className="team_social team_email">
+                    <i className="fa fa-envelope"></i>
+                  </div>
+                </a>
+                <img
+                  className="team_img"
+                  src={`https://gdurl.com${member[3]}`}
+                  alt="."
+                />
               </div>
-              <div className="team_name">{member[1]}</div>
-              <div className="team_desigination">{member[2]}</div>
             </div>
+            <div className="team_name">{member[1]}</div>
+            <div className="team_desigination">{member[2]}</div>
           </div>
-        );
-      }
-    });
-    return Team;
-  };
-  const renderTeam1 = () => {
-    if (TeamData === null) {
-      return;
-    }
-    const Team = TeamData.map((member, i) => {
-      if (i === 0) return "";
-      if (i < 3) {
-        return (
-          <div className="col-md-3" key={i}>
-            <div className="team_card">
-              <div className="team_img_wrap_outer">
-                <div className="team_img_wrap">
-                  <a target="_blank" href={member[5]} rel="noreferrer">
-                    <div className="team_social team_linkedin">
-                      <i className="fa fa-linkedin"></i>
-                    </div>
-                  </a>
-                  <a target="_blank" href={member[4]} rel="noreferrer">
-                    <div className="team_social team_insta">
-                      <i className="fa fa-instagram"></i>
-                    </div>
-                  </a>
-                  <a target="_blank" href={member[6]} rel="noreferrer">
-                    <div className="team_social team_email">
-                      <i className="fa fa-envelope"></i>
-                    </div>
-                  </a>
-                  <img
-                    className="team_img"
-                    src={`https://gdurl.com${member[3]}`}
-                    alt="."
-                  />
-                </div>
-              </div>
-              <div className="team_name">{member[1]}</div>
-              <div className="team_desigination">{member[2]}</div>
-            </div>
-          </div>
-        );
-      }
-    });
-    return Team;
-  };
-  const renderTeam2 = () => {
-    if (TeamData === null) {
-      return;
-    }
-    const Team = TeamData.map((member, i) => {
-      if (i === 0) return "";
-      if (i > 2 && i < 5) {
-        return (
-          <div className="col-md-3" key={i}>
-            <div className="team_card">
-              <div className="team_img_wrap_outer">
-                <div className="team_img_wrap">
-                  <a target="_blank" href={member[5]} rel="noreferrer">
-                    <div className="team_social team_linkedin">
-                      <i className="fa fa-linkedin"></i>
-                    </div>
-                  </a>
-                  <a target="_blank" href={member[4]} rel="noreferrer">
-                    <div className="team_social team_insta">
-                      <i className="fa fa-instagram"></i>
-                    </div>
-                  </a>
-                  <a target="_blank" href={member[6]} rel="noreferrer">
-                    <div className="team_social team_email">
-                      <i className="fa fa-envelope"></i>
-                    </div>
-                  </a>
-                  <img
-                    className="team_img"
-                    src={`https://gdurl.com${member[3]}`}
-                    alt="."
-                  />
-                </div>
-              </div>
-              <div className="team_name">{member[1]}</div>
-              <div className="team_desigination">{member[2]}</div>
-            </div>
-          </div>
-        );
-      }
+        </div>
+      );
     });
     return Team;
   };
@@ -164,14 +76,6 @@ const Teams = () => {
             <p>Meet Our Talented Team Member</p>
           </div>
           <div className="row" style={{ padding: "5%" }}>
-            <div className="col-md-3"></div>
-            {loaded ? renderTeam1() : "Loading..."}
-          </div>
-          <div className="row" style={{ padding: "5%" }}>
-            <div className="col-md-3"></div>
-            {loaded ? renderTeam2() : "Loading..."}
-          </div>
-          <div className="row" style={{ padding: "5%" }}>
             {loaded ? renderTeam() : "Loading..."}
           </div>
         </div>
@@ -181,3 +85,187 @@ const Teams = () => {
 };
 
 export default Teams;
+
+//import { useEffect, useState } from "react";
+
+// //api
+// import axios from "axios";
+
+// //css
+// import "../assets/stylesheets/Teams.css";
+
+// const Teams = () => {
+//   const [TeamData, setTeamData] = useState(null);
+//   const [loaded, setLoaded] = useState(false);
+
+//   useEffect(() => {
+//     axios
+//       .get(
+//         "https://sheets.googleapis.com/v4/spreadsheets/1ZP3Q05AMXjBLXmTKVEUwF05XoEbE4Hi7SN7MOGALMu0/values/Sheet1?key=AIzaSyBrHCkMGLauoHBLQsNWHqMS5w5wp9ZQqhk"
+//       )
+//       .then((res) => {
+//         console.log(res.data.values);
+//         setTeamData(res.data.values);
+//         setLoaded(true);
+//       })
+//       .catch((err) => console.log(err));
+//   }, []);
+
+//   const renderTeam = () => {
+//     if (TeamData === null) {
+//       return;
+//     }
+//     const Team = TeamData.map((member, i) => {
+//       if (i === 0) return "";
+//       if (i > 4) {
+//         return (
+//           <div className="col-md-3" key={i}>
+//             <div className="team_card">
+//               <div className="team_img_wrap_outer">
+//                 <div className="team_img_wrap">
+//                   <a target="_blank" href={member[5]} rel="noreferrer">
+//                     <div className="team_social team_linkedin">
+//                       <i className="fa fa-linkedin"></i>
+//                     </div>
+//                   </a>
+//                   <a target="_blank" href={member[4]} rel="noreferrer">
+//                     <div className="team_social team_insta">
+//                       <i className="fa fa-instagram"></i>
+//                     </div>
+//                   </a>
+//                   <a target="_blank" href={member[6]} rel="noreferrer">
+//                     <div className="team_social team_email">
+//                       <i className="fa fa-envelope"></i>
+//                     </div>
+//                   </a>
+//                   <img
+//                     className="team_img"
+//                     src={`https://gdurl.com${member[3]}`}
+//                     alt="."
+//                   />
+//                 </div>
+//               </div>
+//               <div className="team_name">{member[1]}</div>
+//               <div className="team_desigination">{member[2]}</div>
+//             </div>
+//           </div>
+//         );
+//       }
+//     });
+//     return Team;
+//   };
+//   const renderTeam1 = () => {
+//     if (TeamData === null) {
+//       return;
+//     }
+//     const Team = TeamData.map((member, i) => {
+//       if (i === 0) return "";
+//       if (i < 3) {
+//         return (
+//           <div className="col-md-3" key={i}>
+//             <div className="team_card">
+//               <div className="team_img_wrap_outer">
+//                 <div className="team_img_wrap">
+//                   <a target="_blank" href={member[5]} rel="noreferrer">
+//                     <div className="team_social team_linkedin">
+//                       <i className="fa fa-linkedin"></i>
+//                     </div>
+//                   </a>
+//                   <a target="_blank" href={member[4]} rel="noreferrer">
+//                     <div className="team_social team_insta">
+//                       <i className="fa fa-instagram"></i>
+//                     </div>
+//                   </a>
+//                   <a target="_blank" href={member[6]} rel="noreferrer">
+//                     <div className="team_social team_email">
+//                       <i className="fa fa-envelope"></i>
+//                     </div>
+//                   </a>
+//                   <img
+//                     className="team_img"
+//                     src={`https://gdurl.com${member[3]}`}
+//                     alt="."
+//                   />
+//                 </div>
+//               </div>
+//               <div className="team_name">{member[1]}</div>
+//               <div className="team_desigination">{member[2]}</div>
+//             </div>
+//           </div>
+//         );
+//       }
+//     });
+//     return Team;
+//   };
+//   const renderTeam2 = () => {
+//     if (TeamData === null) {
+//       return;
+//     }
+//     const Team = TeamData.map((member, i) => {
+//       if (i === 0) return "";
+//       if (i > 2 && i < 5) {
+//         return (
+//           <div className="col-md-3" key={i}>
+//             <div className="team_card">
+//               <div className="team_img_wrap_outer">
+//                 <div className="team_img_wrap">
+//                   <a target="_blank" href={member[5]} rel="noreferrer">
+//                     <div className="team_social team_linkedin">
+//                       <i className="fa fa-linkedin"></i>
+//                     </div>
+//                   </a>
+//                   <a target="_blank" href={member[4]} rel="noreferrer">
+//                     <div className="team_social team_insta">
+//                       <i className="fa fa-instagram"></i>
+//                     </div>
+//                   </a>
+//                   <a target="_blank" href={member[6]} rel="noreferrer">
+//                     <div className="team_social team_email">
+//                       <i className="fa fa-envelope"></i>
+//                     </div>
+//                   </a>
+//                   <img
+//                     className="team_img"
+//                     src={`https://gdurl.com${member[3]}`}
+//                     alt="."
+//                   />
+//                 </div>
+//               </div>
+//               <div className="team_name">{member[1]}</div>
+//               <div className="team_desigination">{member[2]}</div>
+//             </div>
+//           </div>
+//         );
+//       }
+//     });
+//     return Team;
+//   };
+
+//   return (
+//     <>
+//       <div className="team-area" id="team">
+//         <br />
+//         <br />
+//         <div className="container team_wrap blob">
+//           <div className="team-heading">
+//             <h1>Team Members</h1>
+//             <p>Meet Our Talented Team Member</p>
+//           </div>
+//           <div className="row" style={{ padding: "5%" }}>
+//             <div className="col-md-3"></div>
+//             {loaded ? renderTeam1() : "Loading..."}
+//           </div>
+//           <div className="row" style={{ padding: "5%" }}>
+//             <div className="col-md-3"></div>
+//             {loaded ? renderTeam2() : "Loading..."}
+//           </div>
+//           <div className="row" style={{ padding: "5%" }}>
+//             {loaded ? renderTeam() : "Loading..."}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Teams;
