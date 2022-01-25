b625c3267d659fede4a63bd6894747266e0ab39d
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2022-01-25 17:41:52 +0530

    Loader and Event section added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+//css
 import "./assets/stylesheets/app.css";
-import Topnav from "./components/Topnav";
+
+//Loader
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+//Components
 import Teams from "./components/Teams";
-import Main from "./components/Main";
-import About from "./components/About";
-// import Discord from "./components/Discord";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
+const Main = lazy(() => import("./components/Main"));
+const Topnav = lazy(() => import("./components/Topnav"));
+const About = lazy(() => import("./components/About"));
+const Event = lazy(() => import("./components/Event"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
+
 const App = () => {
   return (
     <div className="container">
-      <Topnav />
-      <Main />
-      <About />
-      <Teams />
-      <Contact />
-      <Footer />
+      <Suspense fallback={<Loader />}>
+        <Topnav />
+        <Main />
+        <About />
+        <Event />
+        <Teams />
+        <Contact />
+        <Footer />
+      </Suspense>
     </div>
   );
 };
